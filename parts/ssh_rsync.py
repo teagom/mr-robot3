3b18ca16ae36b5c5b1a62bea55f15dc0df82cac3
Tiago SM <teagom@gmail.com>
2021-12-18 13:08:52 -0300

    Fixes begin and end point of paths.

--- parts/ssh_rsync.py
+++ parts/ssh_rsync.py
@@ -8,15 +8,15 @@ def ssh_rsync(x, log, log_err, log_resume, backup_base, backup_mysql, backup_com
     if x[70]: # method
         if x[75] == "password":
             p_lv1('Password authentication')
-            cmd = "sshpass -p \"%s\" rsync %s -e \"ssh -p %s\" %s/* %s@%s:%s" % (x[78], x[74], x[72], backup_base, x[77], x[71], x[73] )
+            cmd = "sshpass -p \"%s\" rsync %s -e \"ssh -p %s\" %s/* %s@%s:%s/." % (x[78], x[74], x[72], backup_base, x[77], x[71], x[73] )
 
         if x[75] == "pemfile":
             p_lv1('Pem file authentication')
-            cmd = "rsync %s -e \"ssh -p %s -i %s\" %s/* %s@%s:%s" % (x[74], x[72], x[46], backup_base, x[77], x[71], x[73])
+            cmd = "rsync %s -e \"ssh -p %s -i %s\" %s/* %s@%s:%s/." % (x[74], x[72], x[46], backup_base, x[77], x[71], x[73])
 
         if x[75] == "authorized":
             p_lv1('Authorized / PubKey authentication')
-            cmd = "rsync %s -e \"ssh -p %s \" %s/* %s@%s:%s" % (x[74], x[72], backup_base, x[77], x[71], x[73])
+            cmd = "rsync %s -e \"ssh -p %s \" %s/* %s@%s:%s/." % (x[74], x[72], backup_base, x[77], x[71], x[73])
 
         cmd_run(cmd, log, log_err)
         log_write(log_resume, ('output %s' % cmd))
